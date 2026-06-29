5cc11d7d3802c1a548aedd84838b5538e6515911
valavayiruchitha-cloud <valavayiruchitha@gmail.com>
2026-06-29 17:32:25 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  
+ int main() {
+    int i = 1 , sum = 0 ;
+    for (i = 1; i <= 100 ; i++)
+    {
+     sum = sum + i ;
+     }
+     printf("sum is : %d\n", sum ); 
+      return 0;
+}
